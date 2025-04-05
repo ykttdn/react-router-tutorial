119115fdff59815bec59e38ede247e26f495c1ed
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-04-05 17:12:00 +0900

    Updating Data

--- app/routes.ts
+++ app/routes.ts
@@ -9,6 +9,10 @@ export default [
   layout("layouts/sidebar.tsx", [
     index("routes/home.tsx"),
     route("contacts/:contactId", "routes/contact.tsx"),
+    route(
+      "contacts/:contactId/edit",
+      "routes/edit-contact.tsx"
+    ),
   ]),
   route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
