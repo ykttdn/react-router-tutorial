16ffbb98a880244a8dd90c05e81752a37842b642
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-04-05 16:00:43 +0900

    Adding an About Route

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,9 @@ export default function App({
   return (
     <>
       <div id="sidebar">
-        <h1>React Router Contacts</h1>
+        <h1>
+          <Link to="about">React Router Contacts</Link>
+        </h1>
         <div>
           <Form id="search-form" role="search">
             <input
