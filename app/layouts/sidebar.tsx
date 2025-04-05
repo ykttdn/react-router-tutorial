333c8aad85e05563b04d6dffe3af021a87dac5b5
ykttdn <38461803+ykttdn@users.noreply.github.com>
2025-04-05 19:12:50 +0900

    URLSearchParams and GET Submissions

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -2,8 +2,12 @@ import { Form, Link, NavLink, Outlet, useNavigation } from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({
+  request,
+}: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
